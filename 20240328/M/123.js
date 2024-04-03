2d0b0ab86f1aba4ad4a6f6f4b4215c7cfc211c4f
weihsuan <hsuan29684@gmail.com>
2024-04-03 15:01:44 +0800

    first commit

---
+++ 20240328/M/123.js
@@ -0,0 +1,19 @@
+/**
+        * 早安
+        */
+const GoodMorning = () => { }
+GoodMorning()
+/**
+ * 早安
+ * @param time 時間 
+ * @param item 物品
+ */
+const GoodMorning2 = (time, item) => { }
+GoodMorning2('aaa', 'bbb')
+/**
+ * 早安
+ * @param time {string} 時間 
+ * @param item {string} 物品
+ */
+const GoodMorning3 = (time, item) => { }
+GoodMorning3('s', 's')
