8d8cbbae3d5f8c7680ddb4d8b1c5308a4f93c57b
weihsuan <hsuan29684@gmail.com>
2024-05-08 16:36:08 +0800

    20240508

---
+++ 20240508Js/M/a.js
@@ -0,0 +1,30 @@
+const a1 = 1
+const a2 = 2
+let a3 = 3
+const a4 = [100, 200]
+const add = () => {
+	return a1 + a2 + a3
+}
+
+const log = () => {
+	console.log('a1: ' + a1)
+	console.log('a2: ' + a2)
+	console.log('a3: ' + a3)
+	console.log('a4: ' + a4)
+}
+
+const test = (value) => {
+	a3 = value
+}
+
+// 一個檔案只能有一個 export default
+// 要匯出多個物件必須使用 { } 和 , 把東西包起來
+export default {
+	a1,
+	a2,
+	a3,
+	a4,
+	add,
+	log,
+	test
+}
