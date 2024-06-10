4ff525d0b5ce249028b430fb6c64e8846b7bd7f9
Weihsuan <hsuan29684@gmail.com>
2024-06-11 01:22:12 +0800

    line機器人更新

---
+++ 20240531/line/commands/flexMessageJson.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import template from '../templates/culture.js'
+
+export default async (event) => {
+	try {
+		console.log('Fetching data from API...')
+		const { data } = await axios.get('https://cultureexpress.taipei/OpenData/Event/C000003')
+		const replies = data.slice(0, 5).map((d) => {
+			const t = template()
+
+			// 檢查並設置回覆內容
+			if (d.Caption) t.body.contents[0].text = d.Caption
+			else t.body.contents[0].text = 'N/A' // 如果 Caption 為空，設置為 'N/A'
+
+			if (d.Company) t.body.contents[1].contents[0].contents[1].text = d.Company
+			else t.body.contents[1].contents[0].contents[1].text = 'N/A' // 如果 Company 為空，設置為 'N/A'
+
+			if (d.StartDate) t.body.contents[1].contents[1].contents[1].text = d.StartDate
+			else t.body.contents[1].contents[1].contents[1].text = 'N/A' // 如果 StartDate 為空，設置為 'N/A'
+
+			if (d.EndDate) t.body.contents[1].contents[2].contents[1].text = d.EndDate
+			else t.body.contents[1].contents[2].contents[1].text = 'N/A' // 如果 EndDate 為空，設置為 'N/A'
+
+			if (d.TicketPrice) t.body.contents[1].contents[3].contents[1].text = d.TicketPrice
+			else t.body.contents[1].contents[3].contents[1].text = 'N/A' // 如果 TicketPrice 為空，設置為 'N/A'
+
+			return t
+		})
+		await event.reply(replies)
+		console.log('Replies sent successfully!')
+	} catch (error) {
+		console.error('Error occurred:', error)
+		await event.reply('發生錯誤')
+	}
+}
--- 20240531/line/index.js
+++ 20240531/line/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config' // 引用 dotenv 套件
 import linebot from 'linebot' // 引用 linebot 套件
 import axios from 'axios' // 引用 axios 套件
 import { handleExhibitionRequest } from './commands/handleExhibitionRequest.js'
-import { handleLectureRequest } from './commands/handleLectureEvents.js'
+import flexMessageJson from './commands/flexMessageJson.js'
 
 // 設定環境變數
 const bot = linebot({
@@ -15,21 +15,25 @@ bot.on('message', async (event) => {
 	console.log(event)
 	if (event.message.type === 'text') {
 		if (event.message.text === '展覽') {
+			// 處理展覽請求
 			await handleExhibitionRequest(event)
-		} else if (event.message.text === '講座') {
-			await handleLectureRequest(event)
+		} else if (event.message.text === '展覽2') {
+			try {
+				// 處理展覽2請求
+				await flexMessageJson(event)
+			} catch (error) {
+				// 捕獲錯誤並回覆預設文本消息
+				console.error('處理展覽2請求時出錯:', error)
+				event.reply({ type: 'text', text: '哩喜勒工沙小2' })
+			}
 		} else {
-			const result = await event.reply({ type: 'text', text: '哩喜勒工沙小' })
+			// 回覆未知命令
+			await event.reply({ type: 'text', text: '哩喜勒工沙小' })
 		}
 	}
 })
 
-// 建立網頁伺服器，去監聽指定路徑進來的請求， process.env.PROT 如果環境變數有 PROT 就使用這個，沒有就使用 3000
-bot.listen('/', process.env.PROT || 3000, () => {
+// 建立網頁伺服器，去監聽指定路徑進來的請求， process.env.PORT 如果環境變數有 PORT 就使用這個，沒有就使用 3000
+bot.listen('/', process.env.PORT || 3000, () => {
 	console.log('機器人啟動')
 })
-// bot.on('message', async (event) => {
-// 	if (event.message.type === 'text') {
-// 		commandFE(event)
-// 	}
-// })
