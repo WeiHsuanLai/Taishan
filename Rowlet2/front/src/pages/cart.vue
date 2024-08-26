331db5e6baffefaaba59405ba0beec920a5217f3
WeiHsuanLai <hsuan29684@gmail.com>
2024-08-27 03:00:20 +0800

    更新

--- Rowlet2/front/src/pages/cart.vue
+++ Rowlet2/front/src/pages/cart.vue
@@ -13,9 +13,9 @@
 
         <!-- 增加和減少房間數量 -->
         <template #[`item.quantity`]="{item}">
-          <v-btn variant="text" color="red" @click="addCart(item.p_id._id, -1,item.date)">-</v-btn>
+          <v-btn variant="text" color="red" @click="addCart(item.p_id._id, -1, item.date)">-</v-btn>
           <span>{{ item.quantity }}</span>
-          <v-btn variant="text" color="green" @click="addCart(item.p_id._id,1,item.date)">+</v-btn>
+          <v-btn variant="text" color="green" @click="addCart(item.p_id._id, 1, item.date)">+</v-btn>
         </template>
 
         <template #[`item.action`]="{item}">
@@ -129,38 +129,40 @@ const checkout = async () => {
   loading.value = false
 }
 
-const addCart = async (product, quantity, date) => {
-  const { data } = await apiAuth.get('/order/all')
-  const lastOrder = data.result[data.result.length - 1]
-  const lastCartItem = lastOrder.cart[0]
-  console.log('aaaaaa', data.result[data.result.length - 1].cart[0].p_id.quantity)
-  let startroom = data.result[data.result.length - 1].cart[0].p_id.quantity
-  data.result.forEach(order => {
-    order.cart.forEach(date => {
-      // console.log('date', date)
-      // console.log('...data.result', data.result[data.result.length - 1]._id)
-      const idid = data.result[data.result.length - 1].cart[0].p_id._id
-      console.log('idid--------', idid)
-      console.log('date---------', date)
-      console.log('date.quantity---------', date.quantity)
-      // console.log('bbbb', data.result[data.result.length - 1].cart[0].quantity)
-      if (date.p_id._id === idid) {
-        const middlroom = date.quantity
-        startroom -= middlroom
-          console.log(startroom)
-        }
-      }
-      )
-  })
+const startRoom = ref(0)
 
+const addCartcc = async (product, quantity, date) => {
+  try {
+    const { data } = await apiAuth.get('/order/all')
+    const lastOrder = data.result[data.result.length - 1]
+    const lastCartItem = lastOrder.cart[0]
+    startRoom.value = lastCartItem.p_id.quantity // 更新 startRoom 的值
+    console.log('Initial startroom value:', startRoom.value)
+
+    data.result.forEach(order => {
+      order.cart.forEach(dateItem => {
+        const idid = lastCartItem.p_id._id
+        if (dateItem.p_id._id === idid) {
+          const middlroom = dateItem.quantity
+          startRoom.value -= middlroom
+          console.log('Updated startroom:', startRoom.value)
+        }
+      })
+    })
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+const addCart = async (product, quantity, date) => {
   const result = await user.addCart(product, quantity, date)
-  createSnackbar({
-    text: result.text,
-    snackbarProps: {
-      color: result.color
-    }
-  })
-  loadItems()
+     console.log('result', result)
+    createSnackbar({
+      text: result.text,
+      snackbarProps: {
+        color: result.color
+      }
+    })
   if (result.color === 'green') {
     const idx = items.value.findIndex(item => item.p_id._id === product)
     console.log('Date parameter:', date) // 確認 date 參數的值
@@ -169,6 +171,142 @@ const addCart = async (product, quantity, date) => {
       items.value.splice(idx, 1)
     }
   }
+  const { data } = await apiAuth.get('/order/all')
+  data.result.forEach(order => {
+    let startroom = data.result[data.result.length - 1].cart[0].p_id.quantity
+    order.cart.forEach(dateItem => {
+      const idid = data.result[data.result.length - 1].cart[0].p_id._id
+      if (dateItem.p_id._id === idid) {
+        const middlroom = dateItem.quantity
+        startroom -= middlroom
+        console.log(startroom)
+      }
+    })
+    }
+  )
 }
+
+// const addCart = async (product, quantity, date) => {
+//   try {
+//     // 發送請求以獲取最新的訂單數據
+//     const { data } = await apiAuth.get('/order/all')
+//     const lastOrder = data.result[data.result.length - 1]
+//     const lastCartItem = lastOrder.cart[0]
+//     let startRoom = lastCartItem.p_id.quantity // 初始化 startRoom 的值
+
+//     // 更新 startRoom 的值
+//     data.result.forEach(order => {
+//       order.cart.forEach(dateItem => {
+//         if (dateItem.p_id._id === lastCartItem.p_id._id) {
+//           startRoom -= dateItem.quantity
+//         }
+//       })
+//     })
+//     console.log('Updated startRoom:', startRoom)
+
+//     // 更新購物車
+//     const result = await user.addCart(product, quantity, date)
+//     console.log('AddCart result:', result)
+//     createSnackbar({
+//       text: result.text,
+//       snackbarProps: {
+//         color: result.color
+//       }
+//     })
+
+//     if (result.color === 'green') {
+//       const idx = items.value.findIndex(item => item.p_id._id === product)
+//       console.log('Date parameter:', date)
+
+//       if (idx !== -1) {
+//         items.value[idx].quantity += quantity
+
+//         if (items.value[idx].quantity <= 0) {
+//           items.value.splice(idx, 1)
+//         }
+//       }
+//     }
+//   } catch (error) {
+//     console.error('Error:', error)
+//   }
+// }
+
+// const addCart = async (product, quantity, date) => {
+//   try {
+//     // 獲取最新的訂單數據
+//     const { data } = await apiAuth.get('/order/all')
+
+//     // 確保 data.result 不為空
+//     if (!data.result || data.result.length !== 0) {
+//       throw new Error('訂單數據無效')
+//     }
+
+//     const lastOrder = data.result[data.result.length - 1]
+
+//     // 確保 lastOrder.cart 存在
+//     if (!lastOrder.cart) {
+//       throw new Error('訂單中沒有購物車數據')
+//     }
+
+//     let startRoom = lastOrder.cart[0]?.p_id?.quantity || 0
+
+//     // 計算現有房間數量
+//     for (const order of data.result) {
+//       if (!order.cart) continue
+//       for (const dateItem of order.cart) {
+//         const idid = lastOrder.cart[0]?.p_id?._id
+//         if (dateItem.p_id._id === idid) {
+//           const middlRoom = dateItem.quantity
+//           startRoom -= middlRoom
+//         }
+//       }
+//     }
+
+//     // 確認是否可以添加更多項目
+//     const currentItem = items.value.find(item => item.p_id._id === product)
+//     const currentQuantity = currentItem ? currentItem.quantity : 0
+//     const newQuantity = currentQuantity + quantity
+
+//     if (newQuantity > startRoom) {
+//       createSnackbar({
+//         text: '無法添加更多，超過可用房間數量',
+//         snackbarProps: {
+//           color: 'red'
+//         }
+//       })
+//       return
+//     }
+
+//     // 添加或更新購物車中的項目
+//     const result = await user.addCart(product, quantity, date)
+//     console.log('result', result)
+//     createSnackbar({
+//       text: result.text,
+//       snackbarProps: {
+//         color: result.color
+//       }
+//     })
+
+//     if (result.color === 'green') {
+//       // 更新 items 的狀態
+//       const idx = items.value.findIndex(item => item.p_id._id === product)
+//       console.log('Date parameter:', date) // 確認 date 參數的值
+//       if (idx !== -1) {
+//         items.value[idx].quantity += quantity
+//         if (items.value[idx].quantity <= 0) {
+//           items.value.splice(idx, 1)
+//         }
+//       } else {
+//         items.value.push({
+//           p_id: { _id: product },
+//           quantity,
+//           date
+//         })
+//       }
+//     }
+//   } catch (error) {
+//     console.error('Error:', error.message || error)
+//   }
+// }
 
 </script>
